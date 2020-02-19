b1032eaad86832ad3ae05cc50b5816ea43de0161
SaneleNgwenya 216019699 <60891151+shakes150e@users.noreply.github.com>
2020-02-19 11:05:56 +0200

    Added the CODE

--- src/com/example/Rep1/Rep1.java
+++ src/com/example/Rep1/Rep1.java
@@ -3,6 +3,9 @@ package com.example.Rep1;
 public class Rep1 {
     public static void main(String[] args) {
     System.out.println("Rep1");
+        
+    System.out.println("GitHub is fun!!!!!!!!!!!");
+    System.out.println("2020 is the year");
     }
 }
 
